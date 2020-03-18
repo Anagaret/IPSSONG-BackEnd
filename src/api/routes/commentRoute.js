2bd649678da525ff85debbedcc3ddec4e9359e54
JeanEmpereur <57395719+JeanEmpereur@users.noreply.github.com>
2020-03-18 23:21:42 +0100

    Revert "Vote"

--- src/api/routes/commentRoute.js
+++
@@ -1,7 +0,0 @@
-module.exports = (server) => {
-  const commentController = require('../controllers/commentController');
-
- server.route('/songs/:song_id/comments') // req.params.song_id
- .get(commentController.list_all_song_comments)
- .post(commentController.create_a_comment);
-}
--- src/api/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-module.exports = (server) => {
-  const userController = require('../controllers/userController');
-
-  server.route('/connection') // req.params.song_id
-  .post(userController.verify_connaction);
-
-  server.route('/inscription')
-  .post(userController.create_one_user);
-}
